588fe01496705033c30d06d50f6232c61cbb6027
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-05-10 01:35:04 +0900

    descriptionの表示方法を改善し、v-forを使用して多言語対応を強化。IntersectionObserverを用いて要素の表示を制御し、アニメーション効果を追加。

--- pages/index.vue
+++ pages/index.vue
@@ -14,26 +14,73 @@
                 </div>
             </div>
             <div class="description">
-                <div class="container">
-                    <h2>{{ $t("desc1_title") }}</h2>
-                    <p>{{ $t("desc1_text") }}</p>
-                </div>
-                <div class="container">
-                    <h2>{{ $t("desc2_title") }}</h2>
-                    <p>{{ $t("desc2_text") }}</p>
-                </div>
-                <div class="container">
-                    <h2>{{ $t("desc3_title") }}</h2>
-                    <p>{{ $t("desc3_text") }}</p>
-                </div>
-                <div class="container">
-                    <h2>{{ $t("desc4_title") }}</h2>
-                    <p>{{ $t("desc4_text") }}</p>
+                <div v-for="(desc, index) in description" :key="index" class="container">
+                    <h2>{{ $t(desc.title) }}</h2>
+                    <p>{{ $t(desc.text) }}</p>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+
+const description = [
+    {
+        title: "desc1_title",
+        text: "desc1_text"
+    },
+    {
+        title: "desc2_title",
+        text: "desc2_text"
+    },
+    {
+        title: "desc3_title",
+        text: "desc3_text"
+    },
+    {
+        title: "desc4_title",
+        text: "desc4_text"
+    }
+]
+
+onMounted(() => {
+    const descElements = document.querySelectorAll('.container');
+    descElements.forEach((descElement) => {
+        // 初期状態で非表示にする
+        descElement.classList.add('hiden');
+    });
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            // entriesはIntersectionObserverEntryの配列
+            // entry.isIntersectingがtrueの時、要素が表示されている
+            // entry.targetはIntersectionObserverEntryのtargetプロパティ
+            // つまり、交差している要素を取得できる
+            // ここで、isDescShownNumをインクリメントする
+            // ただし、最初から表示されている場合も含めて発火するので注意
+            entries.forEach(
+                entry => {
+                    if (entry.isIntersecting) {
+                        // ここが「最初から表示されている」場合も含めて発火する
+                        entry.target.classList.remove('hiden');
+                        observer.unobserve(entry.target);
+                    }
+                })
+        },
+        {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0 // 1.0は、要素が完全に表示されている時
+        }
+    )
+
+    descElements.forEach((descElement) => {
+        observer.observe(descElement);
+    });
+});
+
+</script>
 
 <style scoped lang="scss">
 .content {
@@ -90,7 +137,6 @@
                 justify-content: center;
                 align-items: center;
                 font-weight: bold;
-                margin-top: 50px;
                 cursor: pointer;
                 font-size: 1.5rem;
                 color: $color-body-primary;
@@ -115,6 +161,10 @@
                 margin-bottom: 50px;
                 text-align: left;
 
+                transition: all 1s ease-in-out;
+                transition-delay: 0.2s;
+                opacity: 1;
+
                 &:nth-child(even) {
                     margin-left: auto;
                 }
@@ -127,12 +177,29 @@
                     font-size: 1.5rem;
                     color: $color-body-primary;
                     margin-bottom: 5px;
+
+                    // border-bottom: solid 4px orange;
+                    background: linear-gradient(transparent 50%, $color-accent 80%);
                 }
 
                 p {
                     font-size: 1.2rem;
                     color: $color-body-primary;
                     line-height: 1.5rem;
+                }
+            }
+
+            .container.hiden {
+                opacity: 0;
+                transition: all 0s ease-in-out; // 表示->非表示の時はtransitionを無効にする
+                transition-delay: 0s;
+
+                &:nth-child(even) {
+                    transform: translateX(50px);
+                }
+
+                &:nth-child(odd) {
+                    transform: translateX(-50px);
                 }
             }
         }
