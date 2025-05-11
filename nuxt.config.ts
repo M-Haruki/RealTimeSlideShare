252b083e8836554a328f1f2a983504912e1833a1
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-05-11 14:29:57 +0900

    ssrオプションの位置を修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
+    ssr: true, // default is true
     devtools: { enabled: true },
     modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/i18n"],
     css: ["~/assets/css/main.scss"],
