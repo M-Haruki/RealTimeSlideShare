b24fdf28b33419227b46ca08bce88b4cad6f86b9
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-05-11 14:24:40 +0900

    dbを初期化

--- prisma/migrations/20250511052409_init/migration.sql
+++ prisma/migrations/20250511052409_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Presentations" (
     "presentation_id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "total_page" INTEGER NOT NULL,
-    "current_page" INTEGER NOT NULL,
+    "current_page" INTEGER NOT NULL DEFAULT 0,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -12,5 +12,6 @@ CREATE TABLE "Slides" (
     "uuid" TEXT NOT NULL PRIMARY KEY,
     "presentation_id" TEXT NOT NULL,
     "page" INTEGER NOT NULL,
-    "content" BLOB NOT NULL
+    "content" BLOB NOT NULL,
+    CONSTRAINT "Slides_presentation_id_fkey" FOREIGN KEY ("presentation_id") REFERENCES "Presentations" ("presentation_id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
